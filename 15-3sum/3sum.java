01531173ada4b8c635afb0a536626b927f221397
Mallinath Rampure <rampuremallinath43@gmail.com>
2026-08-04 17:37:19 +0530

    Time: 34 ms (60.89%) | Memory: 59.1 MB (74.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,50 @@
+import java.util.*;
+
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums)
-    {
-        Arrays.sort(nums);    
-        Set<List<Integer>> result=new HashSet<>();
-        for(int i=0;i<nums.length-2;i++)   
-        {
-            int left=i+1;
-            int right=nums.length-1;
+    public List<List<Integer>> threeSum(int[] nums) {
 
-            while(left<right)
-            {
-                int sum=nums[i]+nums[left]+nums[right];
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
+        for (int i = 0; i < nums.length - 2; i++) {
 
-                if(sum==0)
-                {
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
                     left++;
                     right--;
-                }
-                    else if(sum<0)
-                    {
+
+                    // Skip duplicate left values
+                    while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
-                    else
-                    {
+
+                    // Skip duplicate right values
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
             }
-          
         }
-          return new ArrayList<>(result);
+
+        return ans;
     }
 }
