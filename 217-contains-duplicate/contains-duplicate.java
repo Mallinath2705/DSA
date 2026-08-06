3452da0b4dd867a8b66c2845a3b36a1f2af9cde1
Mallinath Rampure <rampuremallinath43@gmail.com>
2026-08-06 10:08:07 +0530

    Time: 14 ms (90.95%) | Memory: 106.3 MB (50.83%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,22 +1,49 @@
 class Solution {
-    public boolean containsDuplicate(int[] nums) 
-    {
-        Arrays.sort(nums);
-        int left=0;
-        int right=left+1;
-    
-        while(left<right && right<nums.length)
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer>duplicate=new HashSet<>();
+
+        for(int num :nums)
         {
-            if(nums[left]==nums[right])
+            if(!duplicate.add(num))
             {
                 return true;
             }
-            else
-            {
-                left++;
-                right++;
-            }
-        }    
+        }
         return false;
+        
+        
     }
 }
+
+
+
+
+
+
+
+
+//Mine ACCCEPTED solution
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) 
+//     {
+//         Arrays.sort(nums);
+//         int left=0;
+//         int right=left+1;
+    
+//         while(left<right && right<nums.length)
+//         {
+//             if(nums[left]==nums[right])
+//             {
+//                 return true;
+//             }
+//             else
+//             {
+//                 left++;
+//                 right++;
+//             }
+//         }    
+//         return false;
+//     }
+// }
