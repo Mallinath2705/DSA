848f46ad6bcc348968e78be0da8544e933993c48
Mallinath Rampure <rampuremallinath43@gmail.com>
2026-09-07 17:16:04 +0530

    Time: 60 ms (43.68%) | Memory: 47.7 MB (49.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,21 +1,77 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) 
     {
-        HashSet<Character>set=new HashSet<>();
-        int left=0;
+        HashSet<Character>sett=new HashSet<>();
+        int low=0;
         int max=0;
+        int n=s.length();
+         for(int high=0;high<n;high++)
+         {  
 
-        for(int right=0;right<s.length();right++)
-        {
-            while(set.contains(s.charAt(right)))
+            while(sett.contains(s.charAt(high)))
             {
-             set.remove(s.charAt(left));
-             left++;   
+                sett.remove(s.charAt(low));
+                low++;
             }
-            set.add(s.charAt(right));
-            max=Math.max(max , right-left+1);
-        }
-        return max;        
-    }
+            sett.add(s.charAt(high));
+            max=Math.max(max , high-low+1);
 
+         }   
+         return max;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) 
+//     {
+//         HashSet<Character>set=new HashSet<>();
+//         int left=0;
+//         int max=0;
+
+//         for(int right=0;right<s.length();right++)
+//         {
+//             while(set.contains(s.charAt(right)))
+//             {
+//              set.remove(s.charAt(left));
+//              left++;   
+//             }
+//             set.add(s.charAt(right));
+//             max=Math.max(max , right-left+1);
+//         }
+//         return max;        
+//     }
+
+// }
