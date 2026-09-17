294216a77faaaa94c7a0d788a4e0900354a346ba
Mallinath Rampure <rampuremallinath43@gmail.com>
2026-09-17 12:36:01 +0530

    Time: 5 ms (98.25%) | Memory: 47.2 MB (13.52%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -2,30 +2,70 @@ class Solution {
     public int firstUniqChar(String s) 
     {
         int n=s.length();
-        int index=-1;
-        HashMap<Character , Integer > mapp=new HashMap<>();
 
+         int [] freq=new int[26];  // 26 size ghetl cause 26 ch letters ahet a to z madhe (small Ones onlyy ! )
+
+         for(int i=0;i<n;i++)
+         {
+            freq[s.charAt(i)-'a']++;
+         }   
         for(int i=0;i<n;i++)
         {
-            char ch=s.charAt(i);
-            if(mapp.containsKey(ch))
+            if(freq[s.charAt(i)-'a']==1)
             {
-                mapp.put(ch,mapp.get(ch)+1);
-            }
-            else
-            {
-                mapp.put(ch,1);
-            }
-        }   
-        for(int i=0;i<n;i++)
-        {
-            char ch=s.charAt(i);
-            if(mapp.get(ch)==1)
-            {
-                index=i;
                 return i;
             }
-        } 
-        return index;
+        }
+            return -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public int firstUniqChar(String s) 
+//     {
+//         int n=s.length();
+//         HashMap<Character , Integer > mapp=new HashMap<>();
+
+//         for(int i=0;i<n;i++)
+//         {
+//             char ch=s.charAt(i);
+//             if(mapp.containsKey(ch))
+//             {
+//                 mapp.put(ch,mapp.get(ch)+1);
+//             }
+//             else
+//             {
+//                 mapp.put(ch,1);
+//             }
+//         }   
+//         for(int i=0;i<n;i++)
+//         {
+//             char ch=s.charAt(i);
+//             if(mapp.get(ch)==1)
+//             {
+//                 return i;
+//             }
+//         } 
+//         return -1;
+//     }
+// }
