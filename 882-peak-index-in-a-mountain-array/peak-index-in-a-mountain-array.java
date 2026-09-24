8c6703cd50ba6d6c72c9c350834ab6f40e756aa7
Mallinath Rampure <rampuremallinath43@gmail.com>
2026-09-24 13:58:36 +0530

    Time: 0 ms (100.00%) | Memory: 80.1 MB (76.06%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) 
+    {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;  
+
+        while(low<high)
+        {
+            int guess=low+(high-low)/2;
+
+            if(arr[guess]<arr[guess+1])
+            {
+                low=guess+1;
+            }
+            else
+            {
+                high=guess;
+            }
+                       
+        }
+        return low;
+    }
+}
